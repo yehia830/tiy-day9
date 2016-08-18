959373e61bb3927c5bfd8b2524f10134e788c537
Yehia Abdullah <Yehia830@Yehias-MacBook-Air.local>
2016-08-18 11:03:40 -0400

    Base version of Day9Runner

---
+++ src/tiyinc/Day9Runner.java
@@ -0,0 +1,11 @@
+package tiyinc;
+
+/**
+ * Created by Yehia830 on 8/18/16.
+ */
+public class Day9Runner {
+    public static void main(String[] args){
+        System.out.println("running");
+    }
+
+}
